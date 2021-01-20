35f153b5c44878b352a75b54dbe7a3da509b79a0
Andrés Vargas <andvargas25@gmail.com>
2021-01-20 15:52:09 -0300

    Se crea function para enviar email

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const express = require("express");
+const cors = require("cors");
+
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.post("/", (req, res) => {
+    const { body } = req;
+    const isValidMessage = body.message && body.to && body.subject;
+
+    if (!isValidMessage) {
+        return res.status(400).send({ message: "invalid request" });
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "consultas@fonoeducar.cl",
+            pass: "Consultas2021"
+        }
+    });
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+    const mailOptions = {
+        from: "consultas@fonoeducar.cl",
+        to: body.to,
+        subject: body.subject,
+        text: body.message
+    };
+
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            return res.status(500).send({ message: "error " + err.message });
+        }
+
+        return res.send({ message: "email sent" });
+    });
+});
+
+module.exports.mailer = functions.https.onRequest(app);
